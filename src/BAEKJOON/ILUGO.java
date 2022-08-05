b053b9ec165d0f31204d833dffebe6d036d4ce37
beak-gu <ngw77@naver.com>
2022-08-05 18:07:32 +0900

    gg

---
+++ src/BAEKJOON/ILUGO.java
@@ -0,0 +1,33 @@
+package BAEKJOON;
+
+import java.io.StringBufferInputStream;
+import java.util.*;
+
+public class ILUGO {
+  public static void main(String[] args){
+
+    Stack<String> stack = new Stack<>();
+
+
+    Scanner sc = new Scanner(System.in);
+    int w = Integer.parseInt(sc.nextLine());
+
+    for(int i=0; i<w; i++){
+      String s = sc.nextLine();
+      String[] arr = s.split(" ");
+      for(int j = 0; j<arr.length; j++){
+        stack.push(arr[j]);
+      }
+      String S1 = "";
+      for(int j = 0; j<arr.length; j++){
+        S1 += stack.pop();
+        if(j == arr.length-1)
+          break;
+        S1 += " ";
+      }
+      System.out.println(S1);
+    }
+
+
+  }
+}
---
+++ src/BAEKJOON/_17608.java
@@ -0,0 +1,4 @@
+package BAEKJOON;
+
+public class _17608 {
+}
